61d5ffb1032191dccd6dad7357bbb9f1202b99d0
kehongying <b06902074@ntu.edu.tw>
2020-04-25 01:20:20 +0800

    update check script

--- check_file/check_order.py
+++ check_file/check_order.py
@@ -6,6 +6,14 @@ file_name = os.listdir(sys.argv[1])
 folder_1, folder_2 = sys.argv[1], sys.argv[2]
 
 for name in file_name:
+	if "dmesg" in name:
+		dmesg = np.argsort(np.genfromtxt(os.path.join(folder_1, name), dtype = np.str, delimiter = " ")[:, 1])
+		stdout = np.argsort(np.genfromtxt(os.path.join(folder_1, name.split("dmesg")[0] + "stdout.txt"), dtype = np.str, delimiter = " ")[:, 0])
+		
+		if np.any(dmesg != stdout):
+			print("%s is crash"%name)
+			print(dmesg, stdout)
+
 	if "stdout" in name:
 		file1 = np.genfromtxt(os.path.join(folder_1, name), dtype = np.str, delimiter = " ")[:, 0]
 		file2 = np.genfromtxt(os.path.join(folder_2, name), dtype = np.str, delimiter = " ")[:, 0]
--- check_file/check_time.py
+++ check_file/check_time.py
@@ -1,3 +1,9 @@
 import sys
+import numpy as np
 
-print(list(map(lambda x: float(x[1]) - float(x[0][:-1]), [lines.split("[Project1] ")[1].split()[1:] for lines in sys.stdin.readlines()])))
+time_log = np.array([lines.split("[Project1] ")[1].split()[1:] for lines in sys.stdin.readlines()]).astype(np.float)
+time = list(map(lambda x: float(x[1]) - float(x[0]), time_log))
+time_log -= np.min(time_log)
+
+for i in range(len(time_log)):
+	print(time_log[i], time[i])
