fcf7afbeb3bb7e5b485c692d8fb7253e1369ffcc
KeHongYing <b06902074@ntu.edu.tw>
2020-04-29 14:26:12 +0800

    update check_order script

--- check_file/check_order.py
+++ check_file/check_order.py
@@ -7,17 +7,18 @@ folder_1, folder_2 = sys.argv[1], sys.argv[2]
 
 for name in file_name:
 	if "dmesg" in name:
-		dmesg = np.argsort(np.genfromtxt(os.path.join(folder_1, name), dtype = np.str, delimiter = " ")[:, 1])
-		stdout = np.argsort(np.genfromtxt(os.path.join(folder_1, name.split("dmesg")[0] + "stdout.txt"), dtype = np.str, delimiter = " ")[:, 0])
+		dmesg1 = np.argsort(np.genfromtxt(os.path.join(folder_1, name), dtype = np.str, delimiter = " ")[:, 1])
+		#stdout = np.argsort(np.genfromtxt(os.path.join(folder_1, name.split("dmesg")[0] + "stdout.txt"), dtype = np.str, delimiter = " ")[:, 0])
+		dmesg2 = np.argsort(np.genfromtxt(os.path.join(folder_2, name), dtype = np.str, delimiter = " ")[:, 1])
 		
-		if np.any(dmesg != stdout):
-			print("%s is crash"%name)
-			print(dmesg, stdout)
+		if np.any(dmesg1 != dmesg2):
+			print("%s dmesg is different"%name)
+			print(dmesg, dmesg1)
 
 	if "stdout" in name:
 		file1 = np.genfromtxt(os.path.join(folder_1, name), dtype = np.str, delimiter = " ")[:, 0]
 		file2 = np.genfromtxt(os.path.join(folder_2, name), dtype = np.str, delimiter = " ")[:, 0]
-		
+
 		if np.any(file1 != file2):
-			print("%s is different"%name)
+			print("%s stdout is different"%name)
 
